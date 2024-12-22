000b4cb2892e3c8b51e3fd5982378c9fc3f7ab53
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2024-12-22 07:34:16 +0600

    Upload js 🥥

--- pages/upload/index.js
+++ pages/upload/index.js
@@ -17,6 +17,7 @@ const UploadPage = () => {
             return res.data
         }
     })
+    // Upload js
     const deleteImage = async (id) => {
         try {
             const res = await api.put(`/upload/delete`, {
