f544c4c0fc23529c67ad8080e798a24e5b6d072a
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2024-12-22 07:50:03 +0600

    Terms and Condition 🎮

--- pages/terms-condition.js
+++ pages/terms-condition.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Layout from "../components/layout/layout";
 import moment from "moment";
-
+// Terms and Condtion
 const TermsAndConditions = () => {
     return (
         <Layout>
