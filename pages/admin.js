3796fe408a935fd66c7ba57ef3335a1eee1f3bc4
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2024-12-22 07:35:51 +0600

    admin js 🍋

--- pages/admin.js
+++ pages/admin.js
@@ -4,7 +4,7 @@ import Categorys from '../components/Setting/Categorys';
 import { useRouter } from 'next/router';
 import Blogs from '../components/Setting/Blogs';
 import Users from '../components/Setting/Users';
-
+// Admin js 
 const settings = () => {
     const query = useRouter().query
     const category = query?.category
